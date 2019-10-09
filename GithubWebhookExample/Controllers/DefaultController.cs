ca284d19dad4122c7e60849958513352d147f023
Omkar Deshpande <odeshpande@tavisca.in>
2019-10-09 11:45:01 +0530

    Done with pull req review comments count

---
+++ GithubWebhookExample/Controllers/DefaultController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using GithubWebhookExample.Github.api;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace GithubWebhookExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("MyPolicy")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Check()
+        {
+            IRestResponse result =ApiCallerWithAuth.CallApi("https://api.github.com/repos/tavisca-odeshpande/WebhooksTest/pulls/1");
+            return Ok();
+        }
+
+        //[HttpPost]
+        //public ActionResult RecievedResponse(Payload payload)
+        //{
+
+
+        //    //var y = Request.Headers["X-Github-Event"];
+
+        //    return Ok();
+        //}
+    }
+
+
+}
---
+++ GithubWebhookExample/Github/webhook/response/models/PullRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GithubWebhookExample.Github.webhook.response.models
+{
+    public class PullRequest
+    {
+        public Uri Url { get; set; }
+    }
+}
